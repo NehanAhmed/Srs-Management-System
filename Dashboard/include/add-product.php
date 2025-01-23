1300403d039a36525e034d68d7ce773eb46635e9
Nehan <nehanahmed2k23@gmail.com>
2025-01-23 11:53:30 +0500

    Nehan

---
+++ Dashboard/include/add-product.php
@@ -0,0 +1,65 @@
+<?php
+include __DIR__ . "/../../Partials/db.php";
+
+
+if (isset($_POST['add-product'])) {
+    if (!empty($_POST['p-code']) && !empty($_POST['r-num']) && !empty($_POST['m-num']) && !empty($_POST['p-type'])) {
+        $p_code = mysqli_real_escape_string($connect, $_POST['p-code']);
+        $r_num = mysqli_real_escape_string($connect, $_POST['r-num']);
+        $m_num = mysqli_real_escape_string($connect, $_POST['m-num']);
+        $p_type = mysqli_real_escape_string($connect, $_POST['p-type']);
+
+function generateProductId() {
+    return mt_rand(1000000000, 9999999999);
+}
+$product_id = generateProductId();
+
+$sql = "INSERT INTO `products`(`product_id`,`product_code`, `revision_number`, `manufacturing_number`, `product_type`) VALUES ('$product_id','$p_code', '$r_num', '$m_num', '$p_type')";
+
+$res = mysqli_query($connect,$sql);
+    if ($res) {
+        
+        echo "<script>alert('Product Added Successfully')</script>";
+    }else{
+        echo "<script>alert('Product Added Failure')</script>";
+
+    }
+
+}
+}
+
+
+?>
+<form action="" method="post">
+<div class="col-12 col-lg-12">
+    <div class="card">
+        
+        <div class="card-body">
+            <h5 class="card-title mb-2">Product Code</h5>
+            <input type="text" class="form-control" name="p-code" placeholder="E.g: 1234556">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title mb-2">Revision Number</h5>
+            <input type="text" class="form-control" name="r-num" placeholder="E.g:3234">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title mb-2">Manufacturing Number</h5>
+            <input type="text" class="form-control" name="m-num" placeholder="E.g: 1344342">
+        </div>
+        <div class="card-body">
+            <h5 class="card-title mb-2">Product Type</h5>
+            <select class="select" name="p-type" required>
+                <option value="">Select</option>
+                <option value="1">Type 1</option>
+                <option value="2">Type 2</option>
+                <option value="3">Type 3</option>
+            </select>
+        </div>
+            <div class="card-body">
+                <input class="btn btn-primary"  type="submit" value="add-product" name="add-product">
+                
+            </div>
+        </div>
+    </div>
+    </form>
+    
---
+++ Partials/sidebar.php
@@ -0,0 +1,80 @@
+<nav id="sidebar" class="sidebar js-sidebar">
+			<div class="sidebar-content js-simplebar" data-simplebar="init"><div class="simplebar-wrapper" style="margin: 0px;"><div class="simplebar-height-auto-observer-wrapper"><div class="simplebar-height-auto-observer"></div></div><div class="simplebar-mask"><div class="simplebar-offset" style="right: 0px; bottom: 0px;"><div class="simplebar-content-wrapper" tabindex="0" role="region" aria-label="scrollable content" style="height: 100%; overflow: hidden scroll;"><div class="simplebar-content" style="padding: 0px;">
+				<a class="sidebar-brand" href="index.html">
+          <span class="align-middle">Srs Management</span>
+        	</a>
+
+				<ul class="sidebar-nav">
+					<li class="sidebar-header">
+						Pages
+					</li>
+
+
+					<li class="sidebar-item active">
+                    <a class="sidebar-link" href="../Dashboard/index.php?dashboard">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sliders align-middle"><line x1="4" y1="21" x2="4" y2="14"></line><line x1="4" y1="10" x2="4" y2="3"></line><line x1="12" y1="21" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="3"></line><line x1="20" y1="21" x2="20" y2="16"></line><line x1="20" y1="12" x2="20" y2="3"></line><line x1="1" y1="14" x2="7" y2="14"></line><line x1="9" y1="8" x2="15" y2="8"></line><line x1="17" y1="16" x2="23" y2="16"></line></svg> <span class="align-middle">Dashboard</span>
+            </a>
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="../Dashboard/index.php?profile">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user align-middle"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> <span class="align-middle">Profile</span>
+            </a>
+					</li>
+
+					
+
+					
+
+					
+
+					<li class="sidebar-header">
+						Tools 
+					</li>
+
+					<li class="sidebar-item">
+						<a class="sidebar-link" href="../Dashboard/index.php?add-products">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle align-middle me-2"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> <span class="align-middle">Add Products</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item">
+                                    <a class="sidebar-link" href="../Dashboard/index.php?test-products">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle align-middle me-2"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg> <span class="align-middle">Test Product</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item">
+                                    <a class="sidebar-link" href="../Dashboard/index.php?cpri">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box align-middle me-2"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg> <span class="align-middle">CPRI</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item">
+                                    <a class="sidebar-link" href="../Dashboard/index.php?marketplace">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-bag align-middle me-2"><path d="M6 2L3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4z"></path><line x1="3" y1="6" x2="21" y2="6"></line><path d="M16 10a4 4 0 0 1-8 0"></path></svg><span class="align-middle">Marketplace</span>
+                        </a>
+					</li>
+
+					
+
+					<li class="sidebar-header">
+						Analytics
+					</li>
+
+					<li class="sidebar-item">
+                                    <a class="sidebar-link" href="../Dashboard/index.php?charts">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bar-chart-2 align-middle"><line x1="18" y1="20" x2="18" y2="10"></line><line x1="12" y1="20" x2="12" y2="4"></line><line x1="6" y1="20" x2="6" y2="14"></line></svg> <span class="align-middle">Charts</span>
+                        </a>
+					</li>
+
+					<li class="sidebar-item">
+                                    <a class="sidebar-link" href="../Dashboard/index.php?report">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text align-middle me-2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg> <span class="align-middle">Report</span>
+                        </a>
+					</li>
+				</ul>
+
+				
+			</div></div></div></div><div class="simplebar-placeholder" style="width: auto; height: 947px;"></div></div><div class="simplebar-track simplebar-horizontal" style="visibility: hidden;"><div class="simplebar-scrollbar" style="width: 0px; display: none;"></div></div><div class="simplebar-track simplebar-vertical" style="visibility: visible;"><div class="simplebar-scrollbar" style="height: 521px; transform: translate3d(0px, 0px, 0px); display: block;"></div></div></div>
+		</nav>
